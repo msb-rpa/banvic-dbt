0135f09fe6bbbe52650fe029d969fc5ee2c23558
MARCELO DE SOUSA BARROS <msb.rpa@gmail.com>
2026-09-07 20:23:54 +0000

    feat: adicionando dim_agencias

---
+++ models/staging/erp_banvic/stg_erp__agencias.sql
@@ -0,0 +1,20 @@
+with 
+    agencias as (
+        select 
+            *
+        from {{ source('erp', 'agencias') }}
+    )
+
+    , renomeado as (
+        select 
+            cod_agencia as pk_agencia
+            , cod_localidade as fk_localidade
+            , nome as nome_agencia
+            , endereco as endereco_agencia            
+            , cast(data_abertura as date) as data_abertura_agencia
+            , tipo_agencia
+        from agencias
+    )
+
+select *
+from renomeado
