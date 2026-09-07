fc609ef7e84234190b85d090eda4fd6ef8d19a43
MARCELO DE SOUSA BARROS <msb.rpa@gmail.com>
2026-09-07 19:58:53 +0000

    feat: adicionando dim_colaboradores

---
+++ models/staging/erp_banvic/stg_erp__colaboradores.sql
@@ -0,0 +1,25 @@
+with
+    colaboradores as (
+        select 
+            * 
+        
+        from {{ source("erp", "colaboradores") }}
+    ),
+
+    renomeado as (
+        select
+            cod_colaborador as pk_colaborador
+            , cod_localidade as fk_localidade
+            , primeiro_nome || ' ' || ultimo_nome as nome_colaborador
+            , email as email_colaborador
+            , regexp_replace(cpf,'[^a-zA-Z0-9]','') as cpf_colaborador
+            , cast(data_nascimento as date) as data_nascimento_colaborador
+            , endereco as endereco_colaborador
+            , regexp_replace(cep,'[^a-zA-Z0-9]','') as cep_colaborador
+            , cod_gerente as cod_gerente_colaborador
+
+        from colaboradores
+    )
+
+select *
+from renomeado
