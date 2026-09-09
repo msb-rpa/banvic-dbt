b2ba091ae79e92e9a7f6f8c726617034babe9cdd
MARCELO DE SOUSA BARROS <msb.rpa@gmail.com>
2026-09-09 01:51:16 +0000

    fct_transacoes finalizada

---
+++ models/intermediate/int_dimensao_contas.sql
@@ -0,0 +1,16 @@
+
+with
+    contas as (
+        select 
+            pk_conta
+            , numero_conta
+            , tipo_conta
+            , ts_abertura_conta
+
+        from {{ ref('stg_erp__contas') }}
+    )
+
+select     
+    *
+
+from contas
